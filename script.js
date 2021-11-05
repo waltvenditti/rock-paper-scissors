bd5efc228705015e46cb0e5e66e7804a6da71f17
WaltVenditi <waltvenditti@gmail.com>
2021-11-05 11:58:19 -0400

    finished more or less

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function playRound(pcPick, manPick) {
     // 0 == draw, 1 == pc wins, -1 == player wins
     let winner = 0;
 
-    updateChoice(manPick, pcPick);
+    
 
     let pc = pcPick.slice(0,1);
     let man = manPick.slice(0,1);
@@ -63,6 +63,8 @@ function playRound(pcPick, manPick) {
             winner = -1;
         }
     } 
+
+    updateChoice(manPick, pcPick, winner);
 
     return winner; 
 }
@@ -103,27 +105,61 @@ function endGame() {
         displayOutcome("COMPUTER WINS GAME")
     };
     removeButtons();
+
+    let replayButton = document.querySelector('#replay');
+    replayButton.style.display = 'inline';
 };
 
 function removeButtons() {
-    let btns = document.querySelectorAll('button');
+    let btns = document.querySelectorAll('.main-container button');
     let btnContainer = document.querySelector('.main-container');
     btns.forEach((button) => {
         button.disabled = true;
     });
 }
 
-function updateChoice(playerChoice, compChoice) {
+function addButtons() {
+    let btns = document.querySelectorAll('.main-container button');
+    let btnContainer = document.querySelector('.main-container');
+    btns.forEach((button) => {
+        button.disabled = false;
+    });
+}
+
+function updateChoice(playerChoice, compChoice, result) {
     let playerChoiceText = document.querySelector('.player-choice .selection');
     let compChoiceText = document.querySelector('.comp-choice .selection');
     playerChoiceText.textContent = playerChoice.toUpperCase();
     compChoiceText.textContent = compChoice.toUpperCase();
+
+    if (result === 1) { // computer wins
+        playerChoiceText.style.color = 'red';
+        compChoiceText.style.color = 'limegreen';
+    } else if (result === -1) {  // player wins
+        playerChoiceText.style.color = 'limegreen';
+        compChoiceText.style.color = 'red';
+    } else {  // draw 
+        playerChoiceText.style.color = 'blue';
+        compChoiceText.style.color = 'blue';
+    };
+
 };
 
 // button listeners
 const btnRock = document.querySelector('#btn-r');
 const btnPaper = document.querySelector('#btn-p');
 const btnScissors = document.querySelector('#btn-s');
+const btnReplay = document.querySelector('#replay');
+
+btnReplay.addEventListener('click', () => {
+    playerScore = 0;
+    compScore = 0;
+    updateChoice('', '', 0);
+    addButtons();
+    displayOutcome('');
+    btnReplay.style.display = 'none';
+    updateScore();
+});
 
 btnRock.addEventListener('click', () => {
     let playerChoice = 'Rock';
